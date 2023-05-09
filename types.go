6b5fc31bcde7cec71ce5b69bad0e45945e44f4c0
Ryan Caught <rcaught@gmail.com>
2023-05-09 23:04:47 +1000

    chore: move types into their own file

---
+++ types.go
@@ -0,0 +1,331 @@
+package filtersql
+
+import (
+	"github.com/samber/lo"
+	"github.com/spf13/cast"
+	"vitess.io/vitess/go/vt/sqlparser"
+)
+
+type Config struct {
+	Allow Allow
+	Debug bool
+}
+
+type Allow struct {
+	Comparisons Comparisons
+	Ors         bool
+	Ands        bool
+}
+
+type (
+	Comparisons []ILeft
+	ILeft       interface {
+		iLeft()
+	}
+	Right interface {
+		iRight()
+		valid(any) bool
+		nodeType() string
+	}
+	Rights []Right
+
+	ComparisonOperators []IComparisonOperator
+	IComparisonOperator interface {
+		iComparisonOperator()
+		ToString() string
+		Rights() Rights
+	}
+)
+
+type Column struct {
+	Qualifier           string
+	Name                string
+	ComparisonOperators ComparisonOperators
+}
+
+func (Column) iLeft() {}
+
+// EqualsOperator
+type (
+	IEqualsOperator interface {
+		iEqualsOperator()
+	}
+	EqualsOperatorRights []IEqualsOperatorRight
+	IEqualsOperatorRight interface {
+		iEqualsOperatorRight()
+		Right() Right
+	}
+	EqualsOperator struct {
+		RightsAccessor EqualsOperatorRights
+	}
+)
+
+func (EqualsOperator) ToString() string     { return "=" }
+func (EqualsOperator) iComparisonOperator() {}
+func (eo EqualsOperator) Rights() Rights {
+	return lo.Map(eo.RightsAccessor, func(item IEqualsOperatorRight, index int) Right {
+		return item.Right()
+	})
+}
+
+// NotEqualsOperator
+type (
+	INotEqualsOperator interface {
+		iNotEqualsOperator()
+	}
+	NotEqualsOperatorRights []INotEqualsOperatorRight
+	INotEqualsOperatorRight interface {
+		iNotEqualsOperatorRight()
+		Right() Right
+	}
+	NotEqualsOperator struct {
+		RightsAccessor NotEqualsOperatorRights
+	}
+)
+
+func (NotEqualsOperator) ToString() string     { return "!=" }
+func (NotEqualsOperator) iComparisonOperator() {}
+func (eo NotEqualsOperator) Rights() Rights {
+	return lo.Map(eo.RightsAccessor, func(item INotEqualsOperatorRight, index int) Right {
+		return item.Right()
+	})
+}
+
+// GreaterThanOperator
+type (
+	IGreaterThanOperator interface {
+		iGreaterThanOperator()
+	}
+	GreaterThanOperatorRights []IGreaterThanOperatorRight
+	IGreaterThanOperatorRight interface {
+		iGreaterThanOperatorRight()
+		Right() Right
+	}
+	GreaterThanOperator struct {
+		RightsAccessor GreaterThanOperatorRights
+	}
+)
+
+func (GreaterThanOperator) ToString() string     { return ">" }
+func (GreaterThanOperator) iComparisonOperator() {}
+func (eo GreaterThanOperator) Rights() Rights {
+	return lo.Map(eo.RightsAccessor, func(item IGreaterThanOperatorRight, index int) Right {
+		return item.Right()
+	})
+}
+
+// LessThanOperator
+type (
+	ILessThanOperator interface {
+		iLessThanOperator()
+	}
+	LessThanOperatorRights []ILessThanOperatorRight
+	ILessThanOperatorRight interface {
+		iLessThanOperatorRight()
+		Right() Right
+	}
+	LessThanOperator struct {
+		RightsAccessor LessThanOperatorRights
+	}
+)
+
+func (LessThanOperator) ToString() string     { return "<" }
+func (LessThanOperator) iComparisonOperator() {}
+func (eo LessThanOperator) Rights() Rights {
+	return lo.Map(eo.RightsAccessor, func(item ILessThanOperatorRight, index int) Right {
+		return item.Right()
+	})
+}
+
+// GreaterThanOrEqualOperator
+type (
+	IGreaterThanOrEqualOperator interface {
+		iGreaterThanOrEqualOperator()
+	}
+	GreaterThanOrEqualOperatorRights []IGreaterThanOrEqualOperatorRight
+	IGreaterThanOrEqualOperatorRight interface {
+		iGreaterThanOrEqualOperatorRight()
+		Right() Right
+	}
+	GreaterThanOrEqualOperator struct {
+		RightsAccessor GreaterThanOrEqualOperatorRights
+	}
+)
+
+func (GreaterThanOrEqualOperator) ToString() string     { return ">=" }
+func (GreaterThanOrEqualOperator) iComparisonOperator() {}
+func (eo GreaterThanOrEqualOperator) Rights() Rights {
+	return lo.Map(eo.RightsAccessor, func(item IGreaterThanOrEqualOperatorRight, index int) Right {
+		return item.Right()
+	})
+}
+
+// LessThanOrEqualOperator
+type (
+	ILessThanOrEqualOperator interface {
+		iLessThanOrEqualOperator()
+	}
+	LessThanOrEqualOperatorRights []ILessThanOrEqualOperatorRight
+	ILessThanOrEqualOperatorRight interface {
+		iLessThanOrEqualOperatorRight()
+		Right() Right
+	}
+	LessThanOrEqualOperator struct {
+		RightsAccessor LessThanOrEqualOperatorRights
+	}
+)
+
+func (LessThanOrEqualOperator) ToString() string     { return "<=" }
+func (LessThanOrEqualOperator) iComparisonOperator() {}
+func (eo LessThanOrEqualOperator) Rights() Rights {
+	return lo.Map(eo.RightsAccessor, func(item ILessThanOrEqualOperatorRight, index int) Right {
+		return item.Right()
+	})
+}
+
+// InOperator
+type (
+	IInOperator interface {
+		iInOperator()
+	}
+	InOperatorRights []IInOperatorRight
+	IInOperatorRight interface {
+		iInOperatorRight()
+		Right() Right
+	}
+	InOperator struct {
+		RightsAccessor InOperatorRights
+	}
+)
+
+func (InOperator) ToString() string     { return "in" }
+func (InOperator) iComparisonOperator() {}
+func (eo InOperator) Rights() Rights {
+	return lo.Map(eo.RightsAccessor, func(item IInOperatorRight, index int) Right {
+		return item.Right()
+	})
+}
+
+// NotInOperator
+type (
+	INotInOperator interface {
+		iNotInOperator()
+	}
+	NotInOperatorRights []INotInOperatorRight
+	INotInOperatorRight interface {
+		iNotInOperatorRight()
+		Right() Right
+	}
+	NotInOperator struct {
+		RightsAccessor NotInOperatorRights
+	}
+)
+
+func (NotInOperator) ToString() string     { return "not in" }
+func (NotInOperator) iComparisonOperator() {}
+func (eo NotInOperator) Rights() Rights {
+	return lo.Map(eo.RightsAccessor, func(item INotInOperatorRight, index int) Right {
+		return item.Right()
+	})
+}
+
+// LiteralValueType
+type (
+	ILiteralValueType interface {
+		iLiteralValueType()
+		valid(any) bool
+	}
+	LiteralValue struct {
+		ValueType ILiteralValueType
+	}
+)
+
+func (LiteralValue) iEqualsOperatorRight()             {}
+func (LiteralValue) iNotEqualsOperatorRight()          {}
+func (LiteralValue) iGreaterThanOperatorRight()        {}
+func (LiteralValue) iLessThanOperatorRight()           {}
+func (LiteralValue) iGreaterThanOrEqualOperatorRight() {}
+func (LiteralValue) iLessThanOrEqualOperatorRight()    {}
+func (LiteralValue) iRight()                           {}
+func (lv LiteralValue) Right() Right                   { return lv }
+func (lv LiteralValue) valid(e any) bool               { return lv.ValueType.valid(e) }
+func (LiteralValue) nodeType() string                  { return "*sqlparser.Literal" }
+
+// StringValue
+type StringValue struct {
+	ValidationFunc func(string) bool
+}
+
+func (StringValue) iLiteralValueType() {}
+func (StringValue) iRight()            {}
+func (sv StringValue) Right() Right    { return sv }
+func (sv StringValue) valid(s any) bool {
+	parent, ok := s.(*sqlparser.Literal)
+	return ok && parent.Type == sqlparser.StrVal && sv.ValidationFunc(parent.Val)
+}
+func (StringValue) nodeType() string { return LiteralValue{}.nodeType() }
+
+// StringValues
+type StringValues struct {
+	ValidationFunc func([]string) bool
+}
+
+func (StringValues) iTupleValueType() {}
+func (StringValues) iRight()          {}
+func (sv StringValues) Right() Right  { return sv }
+func (sv StringValues) valid(s any) bool {
+	parent, ok := s.(sqlparser.ValTuple)
+
+	if ok {
+		values := lo.FilterMap(parent, func(item sqlparser.Expr, index int) (string, bool) {
+			value, ok := item.(*sqlparser.Literal)
+
+			if ok && value.Type == sqlparser.StrVal {
+				return value.Val, true
+			} else {
+				return "", false
+			}
+		})
+
+		if len(values) == len(parent) && sv.ValidationFunc(values) {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+func (StringValues) nodeType() string { return TupleValue{}.nodeType() }
+
+// IntegerValue
+type IntegerValue struct {
+	ValidationFunc func(int) bool
+}
+
+func (IntegerValue) iLiteralValueType() {}
+func (IntegerValue) iRight()            {}
+func (iv IntegerValue) Right() Right    { return iv }
+func (iv IntegerValue) valid(s any) bool {
+	parent, ok := s.(*sqlparser.Literal)
+	return ok && parent.Type == sqlparser.IntVal && iv.ValidationFunc(cast.ToInt(parent.Val))
+}
+func (IntegerValue) nodeType() string { return LiteralValue{}.nodeType() }
+
+// TupleValueType
+type (
+	ITupleValueType interface {
+		iTupleValueType()
+		valid(any) bool
+	}
+	TupleValue struct {
+		ValueType ITupleValueType
+	}
+)
+
+func (TupleValue) iInOperatorRight()    {}
+func (TupleValue) iNotInOperatorRight() {}
+func (TupleValue) iRight()              {}
+func (tv TupleValue) Right() Right      { return tv }
+func (tv TupleValue) valid(e any) bool  { return tv.ValueType.valid(e) }
+func (TupleValue) nodeType() string     { return "sqlparser.ValTuple" }
